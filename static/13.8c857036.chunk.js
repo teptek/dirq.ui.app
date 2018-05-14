70123dd11e59537c5bf0baef158d3b49e12756ef
Tep Tek <teptek@gmail.com>
2018-05-14 10:06:17 +0200

    rounded corners

--- static/13.8c857036.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([13],{783:function(e,t,n){var o=n(15),r=n(45),a=n(289).PageRenderer;a.__esModule&&(a=a.default);var c=r({displayName:"WrappedPageRenderer",getInitialState:function(){return{content:n(798)}},componentWillMount:function(){},render:function(){return o.createElement(a,Object.assign({},this.props,{content:this.state.content}))}});c.__catalog_loader__=!0,e.exports=c},798:function(e,t){e.exports=""}});
-//# sourceMappingURL=13.8c857036.chunk.js.map
